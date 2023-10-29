58d1b69e42f0458cd685ac515ac6b3636433e771
Gathnex <144379642+gathnexadmin@users.noreply.github.com>
2023-10-29 19:16:13 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup
+
+setup(
+    name='Google Translater',  # Replace 'myproject' with your project name
+    version='1.0.0',   # Specify the version of your project
+
+    # Project description
+    description='Free google translater',
+
+    # Author information
+    author='Gokul Raja',
+    author_email='gathnexorg@gmail.com',
+
+    # Project URL
+    url='https://github.com/gathnexadmin/gathtranslate.git',
+
+    # Packages to be included in the distribution
+    packages=['gathtranslater', "__init__.py"],
+
+    install_requires=[
+        'concurrent.futures',
+        'requests',
+        'regex',  # 're' module is included in the regex package
+        'html',
+        'urllib3',
+    ],
+
+    # Other metadata such as license, classifiers, etc.
+    license='MIT',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
